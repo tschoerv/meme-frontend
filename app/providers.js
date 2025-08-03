7376fb11db4a4eab1c9cc049fc963a8146a3150d
tschoerv <tschoerv@protonmail.com>
2025-08-03 12:52:51 +0200

    added rpc fallback to provider

--- app/providers.js
+++ app/providers.js
@@ -4,11 +4,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import {  mainnet } from 'wagmi/chains';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { cookieStorage, createStorage, http } from 'wagmi';
+import { cookieStorage, createStorage, http, fallback } from 'wagmi';
 import { useState, useEffect } from 'react';
 
 const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
 const RPC = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL;
+const RPC_FALLBACK = process.env.NEXT_PUBLIC_INFURA_RPC_URL
 
 export const config = getDefaultConfig({
   appName: 'Meme 2016',
@@ -18,7 +19,10 @@ export const config = getDefaultConfig({
     storage: cookieStorage,
   }),
   transports: {
-    [mainnet.id]: http(RPC),
+    [mainnet.id]: fallback([                                    
+        http(RPC,  { retryCount: 0 }),      // first choice
+        http(RPC_FALLBACK, { retryCount: 0 }),      // silent backup
+      ]),
   },
 });
 
