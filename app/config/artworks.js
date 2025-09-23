d0cb69531ed9110098453447e0cc72991274b96e
tschoerv <tschoerv@protonmail.com>
2025-09-24 00:20:08 +0200

    added artwork for season 1 launch

--- app/config/artworks.js
+++ app/config/artworks.js
@@ -1,14 +1,16 @@
 export const ARTWORKS = {
-  1: { src: '/art/season1/artonymousartifakt_ART.mp4', artist: 'artonymousartifakt', title: 'artonymousartifakt', twitter: 'https://x.com/artonymousart' },
-  2: { src: '', artist: 'Bitcoin', title: '', twitter: 'https://x.com/artistbitcoin' },
-  3: { src: '', artist: 'CryptoArte', title: '', twitter: 'https://x.com/CryptoArte' },
-  4: { src: '', artist: 'DeltaSauce', title: '', twitter: 'https://x.com/delta_sauce' },
-  5: { src: '', artist: 'Metageist', title: '', twitter: 'https://x.com/MetageistVR' },
-  6: { src: '', artist: 'Nuclear Samurai', title: '', twitter: 'https://x.com/MutagenSamurai' },
-  7: { src: '', artist: 'VERDANDI', title: '', twitter: 'https://x.com/TheVERDANDI' },
+  1: { src: '/art/season1/artonymousartifakt_The_DAO.mp4', poster: '/art/season1/optimized/artonymousartifakt_The_DAO-poster-320w.webp', artist: 'artonymousartifakt', title: 'The DAO', pfp:'/artist_pfp/ARTONYMOUSART.webp', twitter: 'artonymousart' },
+  2: { src: '', thumb:'', artist: 'Nuclear Samurai', title: '', pfp:'/artist_pfp/NUCLEARSAMURAI.webp', twitter: 'MutagenSamurai' },
+  3: { src: '', thumb:'', artist: 'CryptoArte', title: '', pfp:'/artist_pfp/CRYPTOARTE.webp', twitter: 'CryptoArte' },
+  4: { src: '', thumb:'', artist: 'DeltaSauce', title: '', pfp:'/artist_pfp/DELTASAUCE.webp', twitter: 'delta_sauce' },
+  5: { src: '', thumb:'', artist: 'Metageist', title: '', pfp:'/artist_pfp/METAGEIST.webp', twitter: 'MetageistVR' },
+  6: { src: '', thumb:'', artist: 'Bitcoin', title: '', pfp:'/artist_pfp/ARTISTBITCOIN.webp', twitter: 'artistbitcoin' },
+  7: { src: '', thumb:'', artist: 'VERDANDI', title: '', pfp:'/artist_pfp/VERDANDI.webp', twitter: 'TheVERDANDI' },
 };
 
 export const PRICE_SEASON_1= 0.01
+
+export const DISCOUNT_PCT_SEASON_1 = 10; //10%
 
 export const EDITION_SEASON_1 = "1/100"
 
