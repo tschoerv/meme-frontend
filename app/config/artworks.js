ac842795c9402d0b076c4580c1dc0059d8c846ab
tschoerv <tschoerv@protonmail.com>
2025-10-03 01:14:38 +0200

    fixed schedule mismatch

--- app/config/artworks.js
+++ app/config/artworks.js
@@ -1,9 +1,9 @@
 export const ARTWORKS = {
   1: { src: '/art/season1/artonymousartifakt_The_DAO.mp4', poster: '/art/season1/optimized/artonymousartifakt_The_DAO-poster-320w.webp', artist: 'artonymousartifakt', title: 'The DAO', pfp:'/artist_pfp/ARTONYMOUSART.webp', twitter: 'artonymousart', drops: "Sept 30th" },
   2: { src: '', thumb:'', artist: 'Nuclear Samurai', title: '', pfp:'/artist_pfp/NUCLEARSAMURAI.webp', twitter: 'MutagenSamurai', drops: "Oct 7th" },
-  3: { src: '', thumb:'', artist: 'CryptoArte', title: '', pfp:'/artist_pfp/CRYPTOARTE.webp', twitter: 'CryptoArte', drops: "Oct 7th" },
-  4: { src: '', thumb:'', artist: 'DeltaSauce', title: '', pfp:'/artist_pfp/DELTASAUCE.webp', twitter: 'delta_sauce', drops: "Oct 14th" },
-  5: { src: '', thumb:'', artist: 'Metageist', title: '', pfp:'/artist_pfp/METAGEIST.webp', twitter: 'MetageistVR', drops: "Oct 21th" },
+  3: { src: '', thumb:'', artist: 'CryptoArte', title: '', pfp:'/artist_pfp/CRYPTOARTE.webp', twitter: 'CryptoArte', drops: "Oct 14th" },
+  4: { src: '', thumb:'', artist: 'DeltaSauce', title: '', pfp:'/artist_pfp/DELTASAUCE.webp', twitter: 'delta_sauce', drops: "Oct 21th" },
+  5: { src: '', thumb:'', artist: 'Metageist', title: '', pfp:'/artist_pfp/METAGEIST.webp', twitter: 'MetageistVR', drops: "Oct 28th" },
   6: { src: '', thumb:'', artist: 'Bitcoin', title: '', pfp:'/artist_pfp/ARTISTBITCOIN.webp', twitter: 'artistbitcoin', drops: "Nov 4th" },
   7: { src: '', thumb:'', artist: 'VERDANDI', title: '', pfp:'/artist_pfp/VERDANDI.webp', twitter: 'TheVERDANDI', drops: "Nov 11th" },
 };
