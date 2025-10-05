b265686754781fdf57eb3d548290ed841c584e84
tschoerv <tschoerv@protonmail.com>
2025-10-05 21:03:40 +0200

    forgot to change back test edit

--- app/config/artworks.js
+++ app/config/artworks.js
@@ -1,6 +1,6 @@
 export const ARTWORKS = {
   1: { src: '/art/season1/artonymousartifakt_The_DAO.mp4', poster: '/art/season1/optimized/artonymousartifakt_The_DAO-poster-320w.webp', artist: 'artonymousartifakt', title: 'The DAO', pfp:'/artist_pfp/ARTONYMOUSART.webp', twitter: 'artonymousart', drops: "Sept 30th", dropsUnix: '1759266000', width:"320", height: "213" },
-  2: { src: '/art/season1/nuclearsamurai_take_me_back.png', thumb:'/art/season1/optimized/nuclearsamurai_take_me_back-320w.webp', artist: 'Nuclear Samurai', title: 'take me back', pfp:'/artist_pfp/NUCLEARSAMURAI.webp', twitter: 'MutagenSamurai', drops: "Oct 7th", dropsUnix: '1759733640', width:"168", height: "252" },
+  2: { src: '/art/season1/nuclearsamurai_take_me_back.png', thumb:'/art/season1/optimized/nuclearsamurai_take_me_back-320w.webp', artist: 'Nuclear Samurai', title: 'take me back', pfp:'/artist_pfp/NUCLEARSAMURAI.webp', twitter: 'MutagenSamurai', drops: "Oct 7th", dropsUnix: '1759870800', width:"168", height: "252" },
   3: { src: '', thumb:'', artist: 'CryptoArte', title: '', pfp:'/artist_pfp/CRYPTOARTE.webp', twitter: 'CryptoArte', drops: "Oct 14th", dropsUnix: '1760475600' },
   4: { src: '', thumb:'', artist: 'DeltaSauce', title: '', pfp:'/artist_pfp/DELTASAUCE.webp', twitter: 'delta_sauce', drops: "Oct 21th", dropsUnix: '1761080400' },
   5: { src: '', thumb:'', artist: 'Metageist', title: '', pfp:'/artist_pfp/METAGEIST.webp', twitter: 'MetageistVR', drops: "Oct 28th", dropsUnix: '1761685200' },
